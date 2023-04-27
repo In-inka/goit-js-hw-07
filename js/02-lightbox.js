00db77d56512e025fa8c8c8e8dc78e40d8077853
Inna Shkilna <shkinai.1995@gmail.com>
2023-04-27 14:03:51 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const imgGallary = galleryItems
+  .map(
+    img =>
+      `
+<li class="gallery__item">
+   <a class="gallery__link" href=${img.original}>
+      <img class="gallery__image" src=${img.preview} alt=${img.description} />
+   </a>
+</li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', imgGallary);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */ captionsData: 'alt',
+  captionDelay: '250',
+});
+
+lightbox.on('show.simplelightbox');
